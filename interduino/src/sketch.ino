a05d6ed92b0a19ff3051e221db102deb0fa4c867
Derek <derek@defiant>
2014-05-24 14:58:31 -0500

    Aruidno project set up

---
+++ interduino/src/sketch.ino
@@ -0,0 +1,22 @@
+// Interduino is the physical manifestation of cigtee - an arduino sketch designed to take serial commands to make stuff happen IRL
+
+
+void setup()
+{
+for(int i=3; i<9; i++) {
+	pinMode(i, OUTPUT);
+	}
+Serial.begin(9600);
+
+}
+
+int pinState[6];
+
+void loop()
+{
+if(Serial.available() > 2) {
+	int pin = Serial.read();
+	pinState[pin] = Serial.read();
+	int duration = Serial.read();
+	}
+}

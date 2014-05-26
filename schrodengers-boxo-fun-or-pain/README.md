f36e4727bb395ba850026f4bd551db8391f4e8ef
Michael Stephens <sudolegit@gmail.com>
2014-05-26 14:19:48 -0500

    :: WIP :: schrodengers-boxo-fun-or-pain :: > Updated 'README'. > Removed WIN code in favor of Linux. > Updated logic to call one of three functions based on button press:   >> 'fun()'         => Attempt to ring buzzer remotely.   >> 'pain()'        => Enter the house of pain [LED in face].   >> 'do_nothing()'  => Just like it sounds, no feedback to user from button press. > NOTE:  The functionality of the 'fun()' and 'pain()' is still pending. Committing before delving inot that.

--- schrodengers-boxo-fun-or-pain/README.md
+++ schrodengers-boxo-fun-or-pain/README.md
@@ -1,11 +1,12 @@
 > This python script polls the keyboard for one of two keypresses:
-  >> (A) INSERT -- 'x52'
-  >> (B) DELETE -- 'x53'
+  >> (A) INSERT
+  >> (B) DELETE
 
 > Each time a button is pressed, a check is done based on a randomly generated 
-  flag. Depending on the result, one of two actions happen:
-   >> (I)  A buzzer is played at SSH via a HTTP request.
-   >> (II) A bright light is flashed in the users face.
+  flag. Depending on the result, one of three possible outcomes occurs:
+   >> (I)   A buzzer is played at SSH via a HTTP request.
+   >> (II)  A bright light is flashed in the users face.
+   >> (III) Nothing Happens.
 
 > Opting to press both buttons simultaneously or in too short a period of time 
   will result in option (II) occurring.
@@ -14,6 +15,10 @@
          and have setup your interpreter for PyDev under the name 'Python_2-7'.
          Once both of those are completed, you should be able to import
          from existing if you choose to debug via Eclipse && PyDev.
+
+> NOTE:  The linux portion was developed on a CENTOS box running Python 2.6.6. 
+         This is expected to function properly on linux boxes running Python
+         2.6.6 - 2.7.X [untested on 3.X].
 
 
 
--- schrodengers-boxo-fun-or-pain/src/schrodengers-boxo-fun-or-pain.py
+++ schrodengers-boxo-fun-or-pain/src/schrodengers-boxo-fun-or-pain.py
@@ -1,30 +1,248 @@
-# IMPORT MODULES.
-import msvcrt
+## IMPORT MODULES REQURED FOR FILE ##
+import os, sys								# OS/System info.
+import tty, termios							# Used for polling keyboard.
+import random, time							# Used for fun or pain randomness && timing.
 
-# DECLARE/INITIALIZE LOCAL VARIABLES.
-special_char_indicator = 'e0'
-button_key_00          = '52'
-button_key_01          = '53'
-key_prev               = ''
 
-# INFINITE LOOP FOR APPLICATION.
-while True:
+
+
+## DEFINE GLOBAL VARIABLES ##
+last_key_press_time	= time.time()			# Time since last key press.
+
+
+
+
+## 
+## FUNCTION DEFINTION => DO NOTHING
+## 
+## PURPOSE:	Mainly a placeholder function. Called when 'nothing' occurs due to button press.
+## 
+## INPUTS:	NONE.
+## 
+## RETURNS:	NONE.
+## 
+def do_nothing():
 	
-	# Check to see if key was pressed.
-	if msvcrt.kbhit():
+	# Print notification message that nothing important happened.
+	print "Nothing To See Here.\r"
+	
+	# :: EXIT ::
+	return
+
+
+
+
+## 
+## FUNCTION DEFINTION => DO NOTHING
+## 
+## PURPOSE:	Mainly a placeholder function. Called when 'nothing' occurs due to button press.
+## 
+## INPUTS:	NONE.
+## 
+## RETURNS:	NONE.
+## 
+def pain():
+    
+	# Print notification message that the pain is beginning.
+	print "Entering The House Of Pain.\r"
+	
+	## TO DO => TOGGLE STATUS OF INSERT [LED] ##
+	
+	# :: EXIT ::
+	return
+
+
+
+
+## 
+## FUNCTION DEFINTION => DO NOTHING
+## 
+## PURPOSE:	Mainly a placeholder function. Called when 'nothing' occurs due to button press.
+## 
+## INPUTS:	NONE.
+## 
+## RETURNS:	NONE.
+## 
+def fun():
+	 
+	# Print notification message that the fun is going to be attempted.
+	print "Attempting Fun Times.\r"
+	
+	## TO DO => TOGGLE STATUS OF NUMLOCK [LED] ##
+	
+	# :: EXIT ::
+	return
+
+
+
+
+## 
+## FUNCTION DEFINTION => FUN OR PAIN
+## 
+## PURPOSE:	Randomly determine if time for fun or pain.
+## 
+## INPUTS:	NONE.
+## 
+## RETURNS:	NONE.
+## 
+def fun_or_pain():
+	
+	# Declare global variables used in function.
+	global last_key_press_time
+	
+	# Declare/Initialize Local Vars.
+	required_wait_time	= 1						# Required time (in seconds) to wait between button presses.
+	
+	# Get current timestamp.
+	timestamp = time.time()
+	
+	# Map functions from random number results to function calls.
+	#  NOTE:  to skew/alter probabilities, simply add extra items to list [unique numbers (indexes) with duplicate functions (vars)].
+	fun_or_pain_chance	= {
+		1 : fun,
+		2 : pain,
+		3 : fun,
+		4 : do_nothing
+	}
+	
+	# :: CHECK :: Verify enough time has elapsed between button presses. ::
+	if (timestamp - last_key_press_time) < required_wait_time:
 		
-		# Read in key value [in hex].
-		key = msvcrt.getch().encode("hex")
+		# Invoke the PAIN.
+		pain()
 		
-		# Verify special key was observed.
-		if(key_prev == special_char_indicator):
+	else:
+		
+		# Get random integer value to determine what should be done:
+		fp = random.randint(fun_or_pain_chance.keys()[0], len(fun_or_pain_chance))
+		
+		# Execute Fun or Pain.
+		fun_or_pain_chance[fp]()
+	
+	# Record current timestamp for reference on next function call.
+	last_key_press_time = timestamp
+	
+	# :: EXIT ::
+	return
+
+
+
+
+## 
+## FUNCTION DEFINTION => POLL KEYBOARD
+## 
+## PURPOSE:	Poll keyboard and return value for every keypress.
+## 
+## INPUTS:	'mode_hex' => When True returns HEX value for character found.
+## 
+## RETURNS:	Character as ascii || hex [based on flag].
+## 
+def poll_kbd(mode_hex=False):
+	
+	# Define file descriptor.
+	fd = sys.stdin.fileno()
+	
+	# Recurrent current file descriptor settings for future reference.
+	fd_settings_old = termios.tcgetattr(fd)
+	
+	# Use try-catch-finally method for grabbing data from keypress.
+	try:
+		
+		# Change mode of file descriptor to raw.
+		tty.setraw(sys.stdin.fileno())
+		
+		# Read in character from standard input.
+		ch = sys.stdin.read(1)
+	
+	finally:
+		
+		# Character read successfully.
+		# Return file descriptor to original settings.
+		termios.tcsetattr(fd, termios.TCSADRAIN, fd_settings_old)
+		
+		# Return value read in.
+		if mode_hex is True:
+			return ch.encode('hex')
+		else:
+			return ch
+	
+	# :: EXIT ::
+	return ""
+
+
+
+
+## 
+## FUNCTION DEFINTION => MAIN APPLICATION
+## 
+## PURPOSE:	Continually poll keyboard and invoke 'fun_or_pain' when a valid key is pressed.
+## 
+## INPUTS:	'mode_hex' => When True returns HEX value for character found.
+## 
+## RETURNS:	Character as ascii || hex [based on flag].
+## 
+def main():
+	
+	# Declare variables used in this function.
+	escape_key				= '03'
+	button_key_sequence_00	= ['1b', '5b', '32', '7e']  # KEY-LIST => INSERT
+	button_key_sequence_01	= ['1b', '5b', '33', '7e']	# KEY-LIST => DELETE
+	button_key_index        = 0x00						# INDEX INTO KEY-LIST
+	
+	# Infinite loop for application.
+	while True:
+		
+		# Poll for keypress.
+		key = poll_kbd(True)
+		
+		# Check to see if 'CTRL+c' [escape] was requested.
+		if key == escape_key:
+			break
+		
+		# :: CHECK :: See if current keypress corresponds to return value from button 00. ::
+		if button_key_sequence_00[button_key_index] == key:
 			
-			# See if current key matches any acceptable values.
-			if(key == button_key_00 or key == button_key_01):
-				print "MATCH 00 " + key
+			# :: CHECK :: See if full sequence has been reached for keypress. ::
+			if (len(button_key_sequence_00) - 1)  == button_key_index:
+				
+				# Key has been found; Reset index.
+				button_key_index = 0x00
+				
+				# Time to see if fun time or pain time.
+				fun_or_pain()
+			
+			# More remains in key list -> increment index.
+			else:
+				button_key_index += 1
 		
-		# Store current keypress for reference later.
-		key_prev = key
+		# :: CHECK :: See if current keypress corresponds to return value from button 01. ::
+		elif button_key_sequence_01[button_key_index] == key:
+			
+			# :: CHECK :: See if full sequence has been reached for keypress. ::
+			if (len(button_key_sequence_01) - 1)  == button_key_index:
+				
+				# Key has been found; Reset index.
+				button_key_index = 0x00
+				
+				# Time to see if fun time or pain time.
+				fun_or_pain()
+			
+			# More remains in key list -> increment index.
+			else:
+				button_key_index += 1
+		
+		# Incorrect key pressed -> reset index.
+		else:
+			button_key_index = 0x00
+	
+	# :: EXIT ::
+	return
+
+
+
+
+## INVOKE MAIN FUNCTION ##
+main()
 
 
 
